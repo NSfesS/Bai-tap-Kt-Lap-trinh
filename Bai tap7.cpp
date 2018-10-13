7b8af254315e73f214e354ad3caa0f39eef30f68
T.Nhat <43803407+NSfesS@users.noreply.github.com>
2018-10-13 10:47:11 +0700

    Update Bai tap7.cpp

--- Bai tap7.cpp
+++ Bai tap7.cpp
@@ -1,23 +1,24 @@
 #include <stdio.h>
-
+//#include <stdlib.h>
 int main()
 {
 	int i,n;
 	float S;
-	printf("tinh tong 1/1*2+1/2*3+...+1/n(n+1)");
+	//int flag=1;
+	printf("tinh tong 1/2+2/3+...+n/n+1");
 	
 	do
 	{
-		printf("\nnhap n: ");
-		scanf("%d",&n);
-		if (n<=0)	printf("\tnhap lai n nhe!");
+			printf("\nnhap n: ");
+			scanf("%d",&n);
+			if (n<=0)	printf("\n\tnhap lai n nhe!");
 	}while(n<=0);
-	 i=1;
+	 i=0;
 	 S=0;
 	while(i<=n)
 	{
-		printf("\n\tS(%d)=%.2f",i,S+=float(1)/(i*(i+1)));
-		i++;	
+		printf("\n\tS(%d)=%.2f",i,S+=float(i)/(i+1));
+		i++;		
 	}
 	printf("\n\t=>S(n)=%.2f",S);
 	
