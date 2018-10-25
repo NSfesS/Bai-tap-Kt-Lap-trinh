1cf632d98429824b1aa76e47a20251714ef7fe05
T.Nhat <43803407+NSfesS@users.noreply.github.com>
2018-10-25 15:37:43 +0700

    Create Bai tap 11.cpp

---
+++ Bai tap 11.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+
+{
+	printf("Nhap S(n)=1+1.2+1.2.3+...+N");
+	int i,n,P,S;
+	do 
+	{
+		printf("\nNhap n: ");
+		scanf("%d",&n);
+		if (n<=0)	printf("\nnhap lai n nhe!\n");
+	}while(n<=0);
+	 i=1;
+	 P=1;
+	 S=0;
+	 
+	  while(i<=n);
+	  {
+			P=P*i;
+			S=S+P;
+			printf("S(%d)=%d",i,S);
+			i++;
+				
+	  }
+	  printf("\n=>S(n)=%d",S);
+
+	return 0; 
+}
