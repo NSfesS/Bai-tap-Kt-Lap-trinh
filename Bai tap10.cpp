ee4a6d211391b2f0aaacd50654e5cc6a1223e982
T.Nhat <43803407+NSfesS@users.noreply.github.com>
2018-10-25 15:13:45 +0700

    Create Bai tap10.cpp

---
+++ Bai tap10.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+
+{
+	int x,n,T;
+		
+	printf("\tT(n)=x^n");
+	
+	 printf("\nNhap gia tri x: ");
+	 scanf("%d",&x);
+	
+	 printf("Nhap gia tri so mu n: ");
+	 scanf("%d",&n);
+	
+	 T=pow(x,n);
+	
+	printf("\t=>T(n)=%d",T);
+	return 0; 
+}
+
+*Cách 2:
+#include <stdio.h>
+#include <math.h>
+int main ()
+
+{
+	float x,n,T;
+		
+	printf("\tT(n)=x^n");
+	
+	 printf("\nNhap gia tri x: ");
+	 scanf("%f",&x);
+	
+	 printf("Nhap gia tri so mu n: ");
+	 scanf("%f",&n);
+	
+	 T=pow(x,n);
+	
+	printf("\t=>T(n)=%.2f",T);
+	return 0; 
+}
