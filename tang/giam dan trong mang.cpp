3548c8c53283dcbde507a7354a2c16dd3c5b4888
T.Nhat <43803407+NSfesS@users.noreply.github.com>
2018-12-12 14:24:32 +0700

    Update and rename tang dan trong mang.cpp to tang/giam dan trong mang.cpp

--- tang/giam dan trong mang.cpp
+++ tang/giam dan trong mang.cpp
@@ -22,7 +22,7 @@ int main ()
 		{
 			if(a[i]>a[j])
 			{
-				temp;
+				temp=a[i];
 				a[i]=a[j];
 				a[j]=temp;
 				//printf("%d",temp);
@@ -35,5 +35,39 @@ int main ()
 		//if(max<a[i]) max=a[i];
 		printf("\t%d",a[i]);
 	}
+	return 0;
+}
+
+//xep thu tu giam dan
+#include<stdio.h>
+int main()
+{
+	int i,n,a[100];
+	printf("nhap gia tri n: ");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("nhap gia tri cua mang a[%d]: ",i);
+		scanf("%d",&a[i]);
+	}
+	int temp;
+	for(i=0;i<n-1;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("\t%d",a[i]);
+	}
+	//printf("\n%d");
 	return 0;
 }
